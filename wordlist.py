4c294dd3fbfce1a4001b9f671e462822826b9098
Kiru <kirushanth.ravindran@gmail.com>
2021-04-23 13:57:49 -0700

    added checkword doctest

--- wordlist.py
+++ wordlist.py
@@ -27,7 +27,15 @@ class WordList:
         return words
 
     def check_word(self, word):
-        """Is word in word list?"""
+        """Is word in word list?
+            
+            >>> wl = WordList("test_dictionary.txt")
+            >>> wl.check_word("CAT")
+            True
+            >>> wl.check_word("RAT")
+            False
+        """
+        
 
         return word in self.words
 
